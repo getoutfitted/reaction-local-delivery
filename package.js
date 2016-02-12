ebe37a9498f7bc8a603ecc535ddd3f05491993df
Spencer Norman <spencern@gmail.com>
2016-02-11 19:20:10 -0700

    Bump versions

--- package.js
+++ package.js
@@ -10,15 +10,14 @@ Package.onUse(function (api) {
   api.use('less');
   api.use('http');
   api.use('underscore');
-  api.use('reactioncommerce:core@0.9.5');
+  api.use('reactioncommerce:core@0.11.0');
   api.use('reactioncommerce:reaction-accounts@1.5.2');
   api.use('iron:router@1.0.12');
   api.use('momentjs:moment@2.10.6');
   api.use('momentjs:twix@0.7.2');
   api.use('standard-minifiers');
   api.use('dburles:factory@0.3.10');
-  api.use('getoutfitted:reaction-advanced-fulfillment@0.5.2');
-  api.use('reactioncommerce:reaction-factories');
+  api.use('getoutfitted:reaction-advanced-fulfillment@0.6.0');
   api.use('underscorestring:underscore.string@3.2.2');
   api.use('pauloborges:mapbox@2.2.3_2');
 
